e98390468a351885819d653e1d2d2de6a9824039
Sean Byfield <seanbyfield@Seans-MacBook-Air.local>
2017-04-01 23:27:29 -0400

    master

---
+++ assets/javascript/keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '8YHXc22OxmwNqF9GhYxFCBtxY',
+  consumer_secret: 'm9koU1rnRo9n5XXKPL0n0Mn6skjqEQPs3y0Oq4NEiCLPdKY68G',
+  access_token_key: '845727341343113216-4iTSZdlZlHhQSndXUgLIdbJMEtx5UvQ',
+  access_token_secret: 'sa8K3COXObkdo7jBttOsgWqUBEqBN3gih5ZG9kWobKWGt',
+}
