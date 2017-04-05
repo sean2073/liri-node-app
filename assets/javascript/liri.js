be3281f93b3673e4aaac8779068eb5da983499ac
Sean Byfield <seanbyfield@Seans-MacBook-Air.local>
2017-04-05 00:35:39 -0400

    master

--- assets/javascript/liri.js
+++ assets/javascript/liri.js
@@ -7,7 +7,7 @@ var consumerSec = fs.twitterKeys.consumer_secret;
 var accessKey = fs.twitterKeys.access_token_key;
 var accessSec = fs.twitterKeys.access_token_secret;
 
-console.log("`	Your consumer key = " + fs.twitterKeys.consumer_key);
+console.log("`  Your consumer key = " + fs.twitterKeys.consumer_key);
 console.log("Your consumer secret = " + fs.twitterKeys.consumer_secret);
 console.log("Your access key = " + fs.twitterKeys.access_token_key);
 console.log("Your access secret = " + fs.twitterKeys.access_token_secret);
@@ -16,77 +16,80 @@ var inCommand = process.argv[2];
 
 if (inCommand === "my-tweets") {
     getTweets();
-} else if (inCommand === "spotify-this-song") {
-    getSpotify();
-} else if (inCommand === "movie-this") {
-    getOMDB();
-} else if (inCommand === "do-what-it-says") {
-    doIt();
-
-    function doIt() {
-        //var fs = require("../text/random.txt");	
-        var fs = require("fs");
-        fs.readFile("../text/random.txt", "utf8", function(error, data) {
-            console.log(data);
-
-            // Then split it by commas (to make it more readable)
-            var dataArr = data.split(",");
-
-            // We will then re-display the content as an array for later use.
-            console.log(dataArr);
-            inCommand = dataArr[0];
-            process.argv[3] = dataArr[1];
-            if (inCommand === "spotify-this-song") {
-
-                getSpotify();
-            } 
+} // close if
+    else if (inCommand === "spotify-this-song") {
+            getSpotify();
+    } // close else if
             else if (inCommand === "movie-this") {
-                getOMDB();
-            } 
-            else if (inCommand === "my-tweets") {
-                getTweets();
-            };
+                    getOMDB();
+            } // close else if
+                    else if (inCommand === "do-what-it-says") {
+                            doIt();
+                } //close else if
 
-            });
-        };
+function doIt() {
+    //var fs = require("../text/random.txt");   
+    var fs = require("fs");
+    fs.readFile("../text/random.txt", "utf8", function(error, data) {
+        console.log(data);
 
-        function getOMDB() {
-            var omdb = require('omdb');
-            var movieName = process.argv[3];
-            var movieYear = process.argv[4];
-            console.log("the movie chosen is " + movieName);
-            if (movieName === ' ' || movieName === undefined) {
-                movieName = 'Mr. Nobody';
-                movieYear = '2009';
-                console.log("the movie chosen is " + movieName);
-            }
-            /* 
+        // Then split it by commas (to make it more readable)
+        var dataArr = data.split(",");
 
-            omdb.search(movieName, function(err, movies) {
-                if(err) {
-                    return console.error(err);
-                } //close if
-             
-                if(movies.length < 1) {
-                    return console.log('No movies were found!');
-                }//closes if
-             
-                movies.forEach(function(movie) {
-                    console.log('%s (%d)', movie.title, movie.year);
-                })//closes movies.foreach
-                });	// closes omdb.search
-                }; //closes getOMDB
-                */
+        // We will then re-display the content as an array for later use.
+        console.log(dataArr);
+        inCommand = dataArr[0];
+        process.argv[3] = dataArr[1];
+        if (inCommand === "spotify-this-song") {
 
-            omdb.get({ title: movieName, year: movieYear }, true, function(err, movie) {
-                if (err) {
-                    return console.error(err);
-                }
+            getSpotify();
+        } // close if
+        else if (inCommand === "movie-this") {
+            getOMDB();
+        } // close else if
+        else if (inCommand === "my-tweets") {
+            getTweets();
+        }; // close else if
 
-                if (!movie) {
-                    return console.log('Movie not found!');
-                }
-                /*
+    }); // close fs.readfile function
+}; // close doIt()
+
+function getOMDB() {
+    var omdb = require('omdb');
+    var movieName = process.argv[3];
+    var movieYear = process.argv[4];
+    console.log("the movie chosen is " + movieName);
+    if (movieName === ' ' || movieName === undefined) {
+        movieName = 'Mr. Nobody';
+        movieYear = '2009';
+        console.log("the movie chosen is " + movieName);
+    } // close if
+    /* 
+    omdb.search(movieName, function(err, movies) {
+        if(err) {
+            return console.error(err);
+        } //close if
+     
+        if(movies.length < 1) {
+            return console.log('No movies were found!');
+        }//closes if
+     
+        movies.forEach(function(movie) {
+            console.log('%s (%d)', movie.title, movie.year);
+        })//closes movies.foreach
+        }); // closes omdb.search
+        }; //closes getOMDB
+        */
+
+    omdb.get({ title: movieName, year: movieYear }, true, function(err, movie) {
+        if (err) {
+            return console.error(err);
+        } // close if
+
+        if (!movie) {
+            return console.log('Movie not found!');
+        } // close if
+        /*
     console.log('%s (%d) %d/10', movie.title);
     console.log('%s (%d) %d/10', movie.year);
     //console.log('%s (%d) %d/10', movie.imdb.rating);
@@ -96,53 +99,54 @@ if (inCommand === "my-tweets") {
     console.log('%s (%d) %d/10', movie.countries);
     console.log(movie.plot);*/
 
-                console.log(movie);
+        console.log(movie);
 
-            }); //closes getOMDB
-        };
+    }); //closes OMDB.get function
+}; //closes getOMDB()
 
-        function getSpotify() {
-            song = process.argv[3];
-            if (song === '') {
-                song = 'The Sign';
-            }
-            var spotify = require('spotify');
+function getSpotify() {
+    song = process.argv[3];
+    if (song === '') {
+        song = 'The Sign';
+    } // close if
+    var spotify = require('spotify');
 
-            spotify.search({ type: 'track', query: song }, function(err, data) {
-                if (err) {
-                    console.log('Error occurred: ' + err);
-                    return;
-                } else {
-                    // Do something with 'data'
-                    //console.log(data); 
-                    var songInfo = data.tracks.items[0];
-                    var songResult = console.log(songInfo.artists[0].name);
-                    console.log(songInfo.name);
-                    console.log(songInfo.album.name);
-                    console.log(songInfo.preview_url);
-                    //console.log(songInfo);
-                }
+    spotify.search({ type: 'track', query: song }, function(err, data) {
+        if (err) {
+            console.log('Error occurred: ' + err);
+            return;
+        } // close if
+        else {
+            // Do something with 'data'
+            //console.log(data); 
+            var songInfo = data.tracks.items[0];
+            var songResult = console.log(songInfo.artists[0].name);
+            console.log(songInfo.name);
+            console.log(songInfo.album.name);
+            console.log(songInfo.preview_url);
+            //console.log(songInfo);
+        } // close else
 
 
-            });
+    }); // close spotify.search function
 
-        }; //closes getSpotify();
-        function getTweets() {
-            var Twitter = require('twitter');
+}; //closes getSpotify();
+function getTweets() {
+    var Twitter = require('twitter');
 
-            var client = new Twitter({
-                consumer_key: consumerKey,
-                consumer_secret: consumerSec,
-                access_token_key: accessKey,
-                access_token_secret: accessSec
-            });
+    var client = new Twitter({
+        consumer_key: consumerKey,
+        consumer_secret: consumerSec,
+        access_token_key: accessKey,
+        access_token_secret: accessSec
+    }); // close twitter object
 
-            var params = { screen_name: 'sean2073', count: 20 };
-            client.get('statuses/user_timeline', params, function(error, tweets, response) {
-                if (!error) {
-                    console.log(tweets);
-                    //console.log(response.text);
-                }
-            });
-        };
-};
+    var params = { screen_name: 'sean2073', count: 20 };
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            console.log(tweets);
+            //console.log(response.text);
+        } // close if
+    }); // close client.get function
+}; // close getTweets
+
